92a2348a209e5c2323a697f160701543ddf60987
NicoleLawson <nicolemlawson1994@gmail.com>
2019-09-18 15:41:49 -0500

    Added PartyBase, CharacterBase, and TileBase

---
+++ Project 1/Assets/Scripts/CharacterBase.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterBase : MonoBehaviour
+{
+    public enum characterType { Rogue, Wizard, Fighter };
+
+    protected characterType type;
+    protected int characterHealth;
+
+
+
+
+
+
+    void Awake()
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public characterType GetCharacterType()
+    {
+        return type;
+    }
+
+    public void SetCharacterType(characterType ct)
+    {
+        type = ct;
+    }
+
+    public int GetCharacterHealth()
+    {
+        return characterHealth;
+    }
+
+    public void SetCharacterHealth(int amount)
+    {
+        characterHealth = amount;
+    }
+}

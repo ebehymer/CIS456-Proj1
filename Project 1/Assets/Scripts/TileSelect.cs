0fc0f50ae07f878ad5c079ac9a1fa391ab38975a
Robyn Riley <rriley818@gmail.com>
2019-09-18 15:38:01 -0500

    Tile Placing

---
+++ Project 1/Assets/Scripts/TileSelect.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileSelect : MonoBehaviour
+{
+    public enum Tile { Enemy, Magic, Trap };
+
+    public Tile type;
+
+    TilePlacementTest man;
+
+    private void Start()
+    {
+        man = GameObject.Find("Tile Manager").GetComponent<TilePlacementTest>(); 
+    }
+
+    private void OnMouseDown()
+    {
+        switch (type)
+        {
+            case Tile.Enemy:
+
+                man.placing = man.enemy;
+                break;
+            case Tile.Magic:
+                man.placing = man.magic;
+                break;
+            case Tile.Trap:
+                man.placing = man.traps;
+                break;
+        }
+    }
+}

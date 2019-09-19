dcfc3c97f5514ea653f1e9c456b9f117ad78f74a
Robyn Riley <rriley818@gmail.com>
2019-09-19 17:39:23 -0500

    Basis for Object Moving on Path

---
+++ Project 1/Assets/Scripts/FollowPath.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class FollowPath : MonoBehaviour
+{
+    public Tilemap path, walked;
+    Vector3Int pos;
+    public Tile floor;
+    public Grid grid;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = FindObjectOfType<Grid>();
+        pos = grid.WorldToCell(GameObject.Find("Start").transform.position);
+        transform.position = grid.GetCellCenterLocal(pos);
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log(pos);
+        if (path.GetTile(pos))
+        {
+            path.SetTile(pos, null);
+            walked.SetTile(pos, floor);
+        }
+        
+    }
+}

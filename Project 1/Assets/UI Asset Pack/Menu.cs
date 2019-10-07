ea8587af69f7b224ad2053107e7e3748d3c4c365
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2019-10-07 15:47:11 -0500

    Some minor changes to things. I added basic menu functionality that has yet to be implemented in the main game

---
+++ Project 1/Assets/UI Asset Pack/Menu.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+
+    private int currentScene;
+    private GameObject pauseMenu;
+    private bool isInMainMenu = false;
+
+    private void Awake()
+    {
+        pauseMenu = GameObject.Find("PauseMenu");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isInMainMenu = true;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        currentScene = SceneManager.GetActiveScene().buildIndex;
+    }
+
+    /// <summary>
+    /// Main Menu
+    /// </summary>
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(currentScene + 1);
+        isInMainMenu = false;
+    }
+
+    /// <summary>
+    /// General Application Buttons
+    /// </summary>
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+
+    /// <summary>
+    /// Pause Menu Section
+    /// </summary>
+
+    public void PauseGame()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape) && !isInMainMenu)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+    }
+
+    public void Resume()
+    {
+        isInMainMenu = false;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+    }
+
+    public void MainMenu()
+    {
+        isInMainMenu = true;
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1;
+        SceneManager.LoadScene(currentScene - 1);
+    }
+}

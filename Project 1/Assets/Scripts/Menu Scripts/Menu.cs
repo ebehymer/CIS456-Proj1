b5a8335b38a35db91fb29452f9ff3f67d4ce03b6
Godofwar1300 <55067215+Godofwar1300@users.noreply.github.com>
2019-10-09 14:55:16 -0500

    Pause Menu and Main Menu set up

--- Project 1/Assets/Scripts/Menu Scripts/Menu.cs
+++ Project 1/Assets/Scripts/Menu Scripts/Menu.cs
@@ -8,7 +8,6 @@ public class Menu : MonoBehaviour
 
     private int currentScene;
     private GameObject pauseMenu;
-    private bool isInMainMenu = false;
 
     private void Awake()
     {
@@ -18,7 +17,6 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        isInMainMenu = true;
         pauseMenu.SetActive(false);
         Time.timeScale = 1;
         currentScene = SceneManager.GetActiveScene().buildIndex;
@@ -31,7 +29,6 @@ public class Menu : MonoBehaviour
     public void StartGame()
     {
         SceneManager.LoadScene(currentScene + 1);
-        isInMainMenu = false;
     }
 
     /// <summary>
@@ -41,33 +38,5 @@ public class Menu : MonoBehaviour
     public void QuitButton()
     {
         Application.Quit();
-    }
-
-    /// <summary>
-    /// Pause Menu Section
-    /// </summary>
-
-    public void PauseGame()
-    {
-        if(Input.GetKeyDown(KeyCode.Escape) && !isInMainMenu)
-        {
-            pauseMenu.SetActive(true);
-            Time.timeScale = 0;
-        }
-    }
-
-    public void Resume()
-    {
-        isInMainMenu = false;
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1;
-    }
-
-    public void MainMenu()
-    {
-        isInMainMenu = true;
-        pauseMenu.SetActive(false);
-        Time.timeScale = 1;
-        SceneManager.LoadScene(currentScene - 1);
     }
 }
